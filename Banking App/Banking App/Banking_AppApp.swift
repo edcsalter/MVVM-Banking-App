d0e5cbdb8529d34f011d6b387cff9b9f6dfb489c
Ed Salter <edcsalter@gmail.com>
2022-03-11 12:56:35 -0500

    Setup

---
+++ Banking App/Banking App/Banking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_AppApp.swift
+//  Banking App
+//
+//  Created by Ed Salter on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Banking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
